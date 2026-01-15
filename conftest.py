115aa8e9a4e7cc8ae8577f5e2375ef62d91d2113
Ando Wikandono <ahmadwikandono@MacBook-Air.local>
2026-01-15 13:36:36 +0700

    updated syntax issue

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture(scope="function")
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.firefox.launch(headless=True)
         context = browser.new_context() # (viewport = { "width": 1920, "height": 1080 })
         page = context.new_page()
         page.goto('https://demoqa.com/automation-practice-form',
